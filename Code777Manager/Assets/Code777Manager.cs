b1ec55f944d940e67f0ed428f90bcc5ac93837fd
pikagamed <51389229+pikagamed@users.noreply.github.com>
2019-06-25 16:35:35 +0800

    Display Tile
    正常顯示TILE

---
+++ Code777Manager/Assets/Code777Manager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Code777Manager : MonoBehaviour
+{
+    public Sprite[] images;
+
+    #region 初始化TILE堆
+    public static Tile[] initialTile = new Tile[28];
+
+    #endregion
+
+    public List<Tile> tilePile = new List<Tile>(28);    //未打開的Tile堆
+    public List<Tile> assistTile = new List<Tile>(28);  //輔助模式下右下角的Tile提示
+    public List<Tile> discardTile = new List<Tile>(28); //使用過被棄置於場中央的Tile堆
+
+    public bool assistMode = true;  //輔助模式，此模式開啟下會於場景右下角提示可能
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        #region  初始化TILE堆
+            initialTile[0] = new Tile(1, new Color(0.00F, 0.60F, 0.25F), images[0]);
+            initialTile[1] = new Tile(2, new Color(0.95F, 0.80F, 0.00F), images[1]);
+            initialTile[2] = new Tile(2, new Color(0.95F, 0.80F, 0.00F), images[1]);
+            initialTile[3] = new Tile(3, new Color(0.25F, 0.25F, 0.25F), images[2]);
+            initialTile[4] = new Tile(3, new Color(0.25F, 0.25F, 0.25F), images[2]);
+            initialTile[5] = new Tile(3, new Color(0.25F, 0.25F, 0.25F), images[2]);
+            initialTile[6] = new Tile(4, new Color(0.65F, 0.30F, 0.15F), images[3]);
+            initialTile[7] = new Tile(4, new Color(0.65F, 0.30F, 0.15F), images[3]);
+            initialTile[8] = new Tile(4, new Color(0.65F, 0.30F, 0.15F), images[3]);
+            initialTile[9] = new Tile(4, new Color(0.65F, 0.30F, 0.15F), images[3]);
+            initialTile[10] = new Tile(5, new Color(0.85F, 0.00F, 0.00F), images[4]);
+            initialTile[11] = new Tile(5, new Color(0.85F, 0.00F, 0.00F), images[4]);
+            initialTile[12] = new Tile(5, new Color(0.85F, 0.00F, 0.00F), images[4]);
+            initialTile[13] = new Tile(5, new Color(0.85F, 0.00F, 0.00F), images[4]);
+            initialTile[14] = new Tile(5, new Color(0.25F, 0.25F, 0.25F), images[5]);
+            initialTile[15] = new Tile(6, new Color(0.85F, 0.10F, 0.80F), images[6]);
+            initialTile[16] = new Tile(6, new Color(0.85F, 0.10F, 0.80F), images[6]);
+            initialTile[17] = new Tile(6, new Color(0.85F, 0.10F, 0.80F), images[6]);
+            initialTile[18] = new Tile(6, new Color(0.00F, 0.60F, 0.25F), images[7]);
+            initialTile[19] = new Tile(6, new Color(0.00F, 0.60F, 0.25F), images[7]);
+            initialTile[20] = new Tile(6, new Color(0.00F, 0.60F, 0.25F), images[7]);
+            initialTile[21] = new Tile(7, new Color(0.95F, 0.80F, 0.00F), images[8]);
+            initialTile[22] = new Tile(7, new Color(0.95F, 0.80F, 0.00F), images[8]);
+            initialTile[23] = new Tile(7, new Color(0.85F, 0.10F, 0.80F), images[9]);
+            initialTile[24] = new Tile(7, new Color(0.00F, 0.35F, 1.00F), images[10]);
+            initialTile[25] = new Tile(7, new Color(0.00F, 0.35F, 1.00F), images[10]);
+            initialTile[26] = new Tile(7, new Color(0.00F, 0.35F, 1.00F), images[10]);
+            initialTile[27] = new Tile(7, new Color(0.00F, 0.35F, 1.00F), images[10]);
+
+            for(int i=0; i<28; i++)
+            {
+                tilePile.Add(initialTile[i]);
+            }
+
+            //AssistMode初始化
+            if (assistMode)
+            {
+                for(int row=0; row<7; row++)
+                {
+                    for(int col=0; col<=row; col++)
+                    {
+                        int i = (row == 0) ? col : ((row) * (row + 1)/ 2 + col);
+                        assistTile.Add(new Tile(tilePile[i].number, tilePile[i].color, 6.5F-row*0.3F+col*0.6F, -2F-row*0.4F, -0.1F - row * 0.1F, true, tilePile[i].image, images[11], "AssistTileObject" + i.ToString(), 0.25F));
+                    }
+                }
+
+               //0
+               //1 -> 1*2/2=1  -> 1+0 1+1
+               //2 -> 2*3/2=3 -> 3+0 3+1 3+2
+               //3 -> 3*4/2=6
+            }
+
+        #endregion
+
+        int drawTile;
+
+        for (int i=0; i<28; i++)
+        {
+            drawTile = Random.Range(0, tilePile.Count);
+            discardTile.Add( new Tile(tilePile[drawTile].number, tilePile[drawTile].color, (i%7)*2F-8F, 3.75F-(i/7)*2.5F, 0,  true, tilePile[drawTile].image, images[11], "TileObject"+i.ToString() ) );
+            tilePile.Remove(tilePile[drawTile]);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Code777GamePlay");
+            }
+        }
+    }
+}
---
+++ Code777Manager/Assets/Tile.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Tile
+{
+    #region 欄位宣告
+
+    private int _number;
+    private Color _color;
+    //Green(0.00, 0.60, 0.25), Yellow(0.95, 0.80, 0.00), Black(0.25, 0.25, 0.25), Brown(0.65, 0.30, 0.15),
+    //Red(0.85, 0.000, 0.000), Pink(0.85, 0.100, 0.80), Blue(0.000, 0.35, 1.000)
+    private float _xPosition;
+    private float _yPosition;
+    private float _zPosition;
+    private Sprite _image;
+    private Sprite _backImage;
+    private bool _openTile;
+    private string _objectName;
+    private GameObject _gameObject;
+
+    #endregion
+
+    #region 屬性設定
+
+    public int number { get { return _number; } }
+    public Color color { get { return _color; } }
+    public float xPosition { get { return _xPosition; } set { _xPosition = value; } }
+    public float yPosition { get { return _yPosition; } set { _yPosition = value; } }
+    public float zPosition { get { return _zPosition; } set { _zPosition = value; } }
+    public Sprite image { get { return _image; } }
+    public Sprite backImage { get { return _backImage; } }
+    public bool openTile { get { return _openTile; } set { _openTile = value; } }
+    public string objectName { get { return _objectName; } }
+
+    #endregion
+
+    #region 建構方法
+
+    /// <summary>
+    /// 類別Tile會於場景生成物件的建構方法
+    /// </summary>
+    /// <param name="number"></param>
+    /// <param name="color"></param>
+    /// <param name="xPosition"></param>
+    /// <param name="yPosition"></param>
+    /// <param name="zPosition"></param>
+    /// <param name="openTile"></param>
+    /// <param name="image"></param>
+    /// <param name="backImage"></param>
+    /// <param name="objectName"></param>
+    public Tile(int number, Color color, float xPosition, float yPosition, float zPosition, bool openTile, Sprite image, Sprite backImage, string objectName, float scale=1F)
+    {
+        _number = number;
+        _color = color;
+        _xPosition = xPosition;
+        _yPosition = yPosition;
+        _zPosition = zPosition;
+        _openTile = openTile;
+        _image = image;
+        _backImage = backImage;
+        _objectName = objectName;
+
+        _gameObject = new GameObject(objectName);
+        _gameObject.AddComponent<SpriteRenderer>();
+        _gameObject.GetComponent<SpriteRenderer>().sprite = openTile ? image : backImage;
+        _gameObject.GetComponent<Transform>().position = new Vector3(xPosition, yPosition, zPosition);
+        _gameObject.GetComponent<Transform>().localScale = new Vector3(scale, scale, scale);
+    }
+
+    /// <summary>
+    /// 類別Tile不於場景生成物件的建構方法
+    /// </summary>
+    /// <param name="number"></param>
+    /// <param name="color"></param>
+    /// <param name="image"></param>
+    public Tile(int number, Color color,  Sprite image)
+    {
+        _number = number;
+        _color = color;
+        _image = image;
+    }
+
+
+    #endregion
+
+}
